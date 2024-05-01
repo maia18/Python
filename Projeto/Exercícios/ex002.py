0044da65742cc477bd7a762c01e98c834a7258a4
maia18 <nelsonkaue05100@gmail.com>
2024-04-30 23:17:32 -0300

    .

---
+++ Projeto/Exercícios/ex002.py
@@ -0,0 +1,10 @@
+value_one = input("Write a value: ")
+value_two = input("Write a value: ")
+
+if value_one >= value_two:
+    print(f"{value_one} is bigger or equal than {value_two}")
+elif value_two >= value_one:
+    print(f"{value_two} is bigger or equal than {value_one}")
+else:
+    print("Values invalids...")
+
