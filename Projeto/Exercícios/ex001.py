8c95df1d3adc78b7ab6c26254874a2320cb124d6
maia18 <nelsonkaue05100@gmail.com>
2024-04-30 22:01:21 -0300

    ,

--- Projeto/Exercícios/ex001.py
+++ Projeto/Exercícios/ex001.py
@@ -1,10 +1,5 @@
-# Nome = 'Kauê'
-# altura_m = 1.88
-# peso_kg = 64
-# imc = ((peso_kg)/(altura_m**2))
-# print(f"Nome: {Nome}, altura: {altura_m}m, peso: {peso_kg}kg, IMC: {imc}kg/m^2")
-
-nome = "Luiz"
-idade = 23
-formato = f'{nome} tem {idade:.2f} anos'
-print(formato)
+Nome = 'Kauê'
+altura_m = 1.88
+peso_kg = 64
+imc = ((peso_kg)/(altura_m**2))
+print(f"Nome: {Nome}, altura: {altura_m}m, peso: {peso_kg}kg, IMC: {imc}kg/m^2")
