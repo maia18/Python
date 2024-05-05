3a19305aea9d7f26eef7630759cf546b231ad73c
maia18 <nelsonkaue05100@gmail.com>
2024-05-04 22:54:17 -0300

    ,

--- Projeto/Exercícios/ex008.py
+++ Projeto/Exercícios/ex008.py
@@ -3,17 +3,25 @@
 while True:
     num_1_ = input("Digit the value one: ")
     num_2_ = input("Digit the value two: ")
+    operation_ = input("Choose the operation ( + , - , * , / ): ")
     if num_1_.isdigit() and num_2_.isdigit():
         num_1 = float(num_1_)
         num_2 = float(num_2_)
-        division_ = input(f"[1]: num_1/num_2 or [2]: num_2/num_1? ")
-        if (int(division_) == 1):
-            if (num_2 != 0):
-                print(f"{num_1}+{num_2}: {(num_1+num_2)}\n{num_1}-{num_2}: {(num_1-num_2)}\n{num_1}*{num_2}: {(num_1*num_2)}\n{num_1}/{num_2}: {(num_1/num_2)}")
-        elif (int(division_) == 2):
-            if (num_1 != 0):
-                print(f"{num_1}+{num_2}: {(num_1+num_2)}\n{num_1}-{num_2}: {(num_1-num_2)}\n{num_1}*{num_2}: {(num_1*num_2)}\n{num_2}/{num_1}: {(num_2/num_1)}")
-
-        choose_ = input("Deseja sair? [s]air [n]ão:").lower().endswith('s')
-        if choose_:
-            break
+        if operation_ == "+":
+            sum = num_1 + num_2
+            print(f"{num_1} {operation_} {num_2}: {sum}")
+        elif operation_ == "-":
+            sub = num_1 - num_2
+            print(f"{num_1} {operation_} {num_2}: {sub}")
+        elif operation_ == "*":
+            product = num_1 * num_2
+            print(f"{num_1} {operation_} {num_2}: {product}")
+        elif operation_ == "/":
+            if num_2 != 0:
+                div = num_1 / num_2
+                print(f"{num_1} {operation_} {num_2}: {div}")
+            else:
+                raise ZeroDivisionError(f"{num_1} {operation_} {num_2}: Divisão inválida!")  
+        choose_ = input("Deseja parar? [s]im [n]ão: ").lower()
+        if choose_ == "s":
+            break 
