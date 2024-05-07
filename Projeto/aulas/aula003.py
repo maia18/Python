b9ff3fabfa1f6eb1e5360c703724f812878b04fe
maia18 <nelsonkaue05100@gmail.com>
2024-05-07 00:00:06 -0300

    h

---
+++ Projeto/aulas/aula003.py
@@ -0,0 +1,43 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        +01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres (len)
+# print(bool([]))  # falsy
+# print(lista, type(lista))
+
+#        0    1      2              3    4
+#       -5   -4     -3             -2   -1
+lista = [123, True, 'Luiz Otávio',  1.2, []]
+print(lista)
+print(lista[3], type(lista[3]))
+lista[-3] = 'Maria'
+print(lista)
+print(lista[2], type(lista[2]))
+
+#        0   1   2   3      4       5
+lista = [10, 20, 30, 40, 'marcos', 4.3]
+lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(5)
+print(lista, 'Removido,', ultimo_valor)
