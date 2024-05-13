e6170cabb9564875f0e432590702dd388851be0e
maia18 <nelsonkaue05100@gmail.com>
2024-05-13 10:34:25 -0300

    ...

---
+++ Projeto/aulas/aula007.py
@@ -0,0 +1,34 @@
+"""
+Interpretador do Python
+
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c 'cmd' (comando)
+python -i mod.py (interativo com mod)
+
+The Zen of Python, por Tim Peters
+
+Bonito é melhor que feio.
+Explícito é melhor que implícito.
+Simples é melhor que complexo.
+Complexo é melhor que complicado.
+Plano é melhor que aglomerado.
+Esparso é melhor que denso.
+Legibilidade conta.
+Casos especiais não são especiais o bastante para quebrar as regras.
+Embora a praticidade vença a pureza.
+Erros nunca devem passar silenciosamente.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Deve haver um -- e só um -- modo óbvio para fazer algo.
+Embora esse modo possa não ser óbvio à primeira vista a menos que você seja holandês.
+Agora é melhor que nunca.
+Embora nunca frequentemente seja melhor que *exatamente* agora.
+Se a implementação é difícil de explicar, é uma má ideia.
+Se a implementação é fácil de explicar, pode ser uma boa ideia.
+Namespaces são uma grande ideia -- vamos fazer mais dessas!
+"""
+
+print(1+1) ; print(2+2) ; print(3+3) ; print(4+4) ; print(5+5)
+print(6+6) ; print(7+7) ; print(8+8) ; print(9+9) ; print(10+10)
