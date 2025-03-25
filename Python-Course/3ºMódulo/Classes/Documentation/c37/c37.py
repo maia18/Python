fb485c57f9e663d7ea450bebbda4729ac3a59754
maia18 <nelsonkaue05100@gmail.com>
2025-03-25 15:45:28 -0300

    16_2ºclass

--- Python-Course/3ºMódulo/Classes/Documentation/c37/c37.py
+++ Python-Course/3ºMódulo/Classes/Documentation/c37/c37.py
@@ -2,19 +2,49 @@
 # Enumerações na programação são usadas em ocasiões onde temos um determinado número de coisas para escolher.
 # Enums têm membros e seus valores são constantes.
 # Enums em python:
-#   - são um conjunto de nomes simbólicos (membros) ligados a valores únicos
-#   - podem ser iterados para retornar seus membros canônicos na ordem de definição
+#   - são um conjunto de nomes simbólicos (membros) ligados a valores únicos;
+#   - podem ser iterados para retornar seus membros canônicos na ordem de definição.
 # enum.Enum é a superclasse para suas enumerações. Mas também pode ser usada diretamente (mesmo assim, Enums não são classes normais em Python).
 # Você poderá usar seu Enum com type annotations, com isinstance e outras coisas relacionadas com tipo.
 # Para obter os dados:
 #   • membro = Classe(valor), Classe['chave']
 #   • chave = Classe.chave.name
 #   • valor = Classe.chave.value
-def mover(direcao):
-    print(f'Movendo para {direcao}')
+import enum
+
+# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
 
 
-mover('esquerda')
-mover('direita')
-mover('acima')
-mover('abaixo')
+class Direções(enum.Enum):
+    ESQUERDA =  enum.auto()
+    DIREITA  =  enum.auto()
+    ACIMA    =  enum.auto()
+    ABAIXO   =  enum.auto()
+
+
+print(Direções(1), 
+      Direções['ESQUERDA'], 
+      Direções.ESQUERDA, 
+      Direções(1).name, 
+      f'({Direções.ESQUERDA.value})',
+      sep='\n')
+
+print(Direções(4), 
+      Direções['ABAIXO'], 
+      Direções.ABAIXO, 
+      Direções(4).name,
+      f'({Direções.ABAIXO.value})',
+      sep='\n')
+
+
+def mover(direcao: Direções):
+    if not isinstance(direcao, Direções):
+        raise ValueError('Direção não encontrada')
+
+    print(f'Movendo para {direcao.name} ({direcao.value})')
+
+
+mover(Direções.ESQUERDA)
+mover(Direções.DIREITA)
+mover(Direções.ACIMA)
+mover(Direções.ABAIXO)
