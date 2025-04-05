1bfa69d0454b05428fa9bb4f8fb97a6a81db1f9e
maia18 <nelsonkaue05100@gmail.com>
2025-04-05 10:28:08 -0300

    21_4ºclass

--- Python-Course/4ºMódulo/Classes/Pillow/c34.py
+++ Python-Course/4ºMódulo/Classes/Pillow/c34.py
@@ -6,16 +6,18 @@ from PIL import Image
 
 ROOT_FOLDER = Path(__file__).parent
 ORIGINAL  = ROOT_FOLDER / 'original.JPG'
-NEW_IMAGE = ROOT_FOLDER / ' new.JPG'
+NEW_IMAGE = ROOT_FOLDER / 'new.JPG'
 
 pil_image = Image.open(ORIGINAL)
+
 width, height = pil_image.size
 exif = pil_image.info['exif']
 
-# width     new_width
-# height    ??
-new_width = 640
-new_height = round(height * new_width / width)
+# width      new_width
+# height     ??
+
+new_width  = 640
+new_height = round((height * new_width) / width)
 
 new_image = pil_image.resize(size=(new_width, new_height))
 new_image.save(
