c4cfe759a1ac9241cbdb9774e86f7ba4e5b42219
maia18 <nelsonkaue05100@gmail.com>
2025-04-06 16:34:16 -0300

    24ºclass

---
+++ Python-Course/5ºMódulo/Classes/PySide6/Calculator/main.py
@@ -0,0 +1,21 @@
+import sys
+
+from main_window import MainWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from variables import WINDOW_ICON_PATH
+
+if __name__ == '__main__':
+    # Cria a aplicação
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    # Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+    # Executa tudo
+    window.adjustFixedSize()
+    window.show()
+    app.exec()
